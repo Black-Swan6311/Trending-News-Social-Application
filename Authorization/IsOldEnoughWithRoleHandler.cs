0b757bc784ca95840e8b9bf58f26599a543d00db
Black-Swan6311 <115357477+Black-Swan6311@users.noreply.github.com>
2022-12-06 15:45:55 -0500

    Add files via upload

---
+++ Authorization/IsOldEnoughWithRoleHandler.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TrendingNews.Authorization
+{
+    public class IsOldEnoughWithRoleHandler : AuthorizationHandler<IsOldEnoughWithRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsOldEnoughWithRoleRequirement requirement)
+        {
+            var bDay = context.User.Claims.Where(x => x.Type == ClaimTypes.DateOfBirth).FirstOrDefault().Value;
+            var bDayObj = Convert.ToDateTime(bDay);
+
+            DateTime now = DateTime.Today;
+            int age = DateTime.Today.Year - bDayObj.Year;
+            if (now < bDayObj.AddYears(age))
+                age--;
+
+            if (requirement.MinAge <= age && context.User.IsInRole("Admin"))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
